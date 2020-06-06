e738478ec7ba6d86fdc41f3b125228169bb7a963
jinxbunny <info@jmkit.com>
2020-06-06 16:42:51 +0100

    first commit

---
+++ src/components/Welcome.js
@@ -0,0 +1,17 @@
+import { Component} from 'react';
+import b1 from './img/friendlybot.png';
+import b2 from './img/mehbot.png';
+
+class Welcome extends Component {
+
+    render {
+        return (
+        <div className="AppHeader">
+          <h1>Portfolio</h1>
+          <h3>Robert Marsh</h3>
+        </div>
+      );
+    }
+}
+
+export default Welcome;
