e6e9faee303a665d499fcc41de0fa088061ebb9a
jinxbunny <info@jmkit.com>
2020-06-06 16:47:20 +0100

    fixed welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,10 +1,8 @@
 import { Component} from 'react';
-import b1 from './img/friendlybot.png';
-import b2 from './img/mehbot.png';
 
 class Welcome extends Component {
 
-    render {
+    render (){
         return (
         <div className="AppHeader">
           <h1>Portfolio</h1>
