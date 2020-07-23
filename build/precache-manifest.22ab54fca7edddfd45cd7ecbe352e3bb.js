dc0055032b4ffad1d39052b1c931da38565f5b08
Robert <info@jmkit.com>
2020-07-23 17:36:43 +0100

    checking in latest images and css

---
+++ build/precache-manifest.22ab54fca7edddfd45cd7ecbe352e3bb.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ed919506f74b2e8e31c7f33e754d66",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a6b56f1fce443c6313a8",
+    "url": "/portfolio/static/css/main.c967c796.chunk.css"
+  },
+  {
+    "revision": "8bffda2176da6d93a3d9",
+    "url": "/portfolio/static/js/2.e865bdb7.chunk.js"
+  },
+  {
+    "revision": "8bc88370bc2489d86f2f357f447ddb0b",
+    "url": "/portfolio/static/js/2.e865bdb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6b56f1fce443c6313a8",
+    "url": "/portfolio/static/js/main.a30dc448.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "c753fc2a05fd45d57ee1b78d036f96f5",
+    "url": "/portfolio/static/media/anchor.c753fc2a.png"
+  },
+  {
+    "revision": "ac1890304fdabfd7e0d40e580dd2d41b",
+    "url": "/portfolio/static/media/cage.ac189030.png"
+  },
+  {
+    "revision": "dc77c19b03cde99399eed69477863fc3",
+    "url": "/portfolio/static/media/cyberpet.dc77c19b.png"
+  },
+  {
+    "revision": "1a7f0a82a528b8d808cd4f771806bbad",
+    "url": "/portfolio/static/media/dicegame.1a7f0a82.png"
+  },
+  {
+    "revision": "c70b72ee434e8d07e73976e0cfdc51ad",
+    "url": "/portfolio/static/media/drag-left.c70b72ee.gif"
+  },
+  {
+    "revision": "fffe7f5a13dfaedb3fdb723b75b9cef6",
+    "url": "/portfolio/static/media/express.fffe7f5a.png"
+  },
+  {
+    "revision": "930d335f9d0ec3ea53fc43287a997ef8",
+    "url": "/portfolio/static/media/mongo.930d335f.png"
+  },
+  {
+    "revision": "6c8a9665ee8a3e251bad53748815cedd",
+    "url": "/portfolio/static/media/node.6c8a9665.png"
+  },
+  {
+    "revision": "d7361ab64acb43ef05b4f073bc6b55de",
+    "url": "/portfolio/static/media/profile.d7361ab6.png"
+  },
+  {
+    "revision": "ed931db8893a909300aa1b8c2c81aab2",
+    "url": "/portfolio/static/media/react.ed931db8.png"
+  },
+  {
+    "revision": "6a064d0a10ef5b3aad9ff7a2adecba8c",
+    "url": "/portfolio/static/media/slackproject.6a064d0a.png"
+  },
+  {
+    "revision": "132e2241d42081e2f61b7d06e9f4ce10",
+    "url": "/portfolio/static/media/sql.132e2241.png"
+  },
+  {
+    "revision": "f258affb56ca97f017a2f6130a7d3d84",
+    "url": "/portfolio/static/media/vsc.f258affb.png"
+  }
+]);
