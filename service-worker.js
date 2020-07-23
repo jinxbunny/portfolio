33721b8aa5d6548152350de4087cda9082caa6e9
Robert <info@jmkit.com>
2020-07-23 17:33:18 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.b7d5b1af1000bf86e01b895172764395.js"
+  "/portfolio/precache-manifest.22ab54fca7edddfd45cd7ecbe352e3bb.js"
 );
 
 self.addEventListener('message', (event) => {
